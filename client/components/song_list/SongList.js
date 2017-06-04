965f65c860dd9de4ab97409165a0bd5653d81362
Marek Melichar <koprivackej@gmail.com>
2017-06-04 14:41:19 +0200

    SongList try 2

--- client/components/song_list/SongList.js
+++ client/components/song_list/SongList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
-import query from '../../queries/fetchSongs';
+// import query from '../../queries/fetchSongs';
 
 class SongList extends Component {
   onSongDelete(id) {
